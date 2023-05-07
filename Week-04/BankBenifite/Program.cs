5de3afb970950d4ce132bef52b2aa004f75757bd
Milad <130595306+M-Ahmadi-2002@users.noreply.github.com>
2023-05-07 11:16:28 +0430

    Add files via upload

---
+++ Week-04/BankBenifite/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankBenifite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Blc, BG, BB;
+            int month_Waite=0;
+            Console.WriteLine("enter blance");
+            Blc = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter Gold");
+            BG = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter Bank Benifite");
+            BB = Convert.ToDouble(Console.ReadLine());
+
+            while(Blc<=BG)
+            {
+                Blc = (Blc * (BB / 100)) + Blc;
+                month_Waite++;
+                Console.WriteLine("you blace after \t"+month_Waite+"\t is \t"+Blc);
+            }
+            //Console.WriteLine("you blance after one month: ");
+            //BG = (Blc * (BB / 100)) + Blc;
+
+            Console.WriteLine("Number of mounth thay you should waite is \t"+ month_Waite);
+           
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Week-04/Branching/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+            char op;
+            //string req;
+            //Console.WriteLine("enter your request");
+            //req = Console.ReadLine();
+            //req.Split('+', '-', '/' ,'*');
+            //Console.WriteLine("enter first number");
+            //a = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("enter second number");
+            //b = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("enter an operator");
+            //op = Convert.ToChar(Console.ReadLine());
+            //if (op == '+')
+            //    Console.WriteLine(a + b);
+            //else if (op == '-')
+            //    Console.WriteLine(a - b);
+            //else if (op == '*')
+            //    Console.WriteLine(a * b);
+            //else if (op == '/')
+            //    Console.WriteLine(a / b);
+            //else
+            //    Console.WriteLine("ERRORE");
+            switch(op)
+            {
+                case '+':
+                    {
+                        Console.WriteLine(a + b);
+                        break;
+                    }
+                case '-':
+                    {
+                        Console.WriteLine(a - b);
+                        break;
+                    }
+                case '/':
+                    {
+                        if (b == 0)
+                            Console.WriteLine("ERRORE");
+                        else
+                        Console.WriteLine(a / b);
+                        break;
+                    }
+                case '*':
+                    {
+                        Console.WriteLine(a * b);
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("ERRORE");
+                        break;
+                    }
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
